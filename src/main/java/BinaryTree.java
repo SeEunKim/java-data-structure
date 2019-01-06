04f8309bd5990d15644c113f9c11dcd4dcdbe7e0
kimse9450@gmail.com <kimse9450@gmail.com>
2019-01-06 23:10:20 +0900

    feat : BinaryTree

---
+++ src/main/java/BinaryTree.java
@@ -0,0 +1,70 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+class Tree {
+    public Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public Node makeNode(Node left, int data, Node right) {
+        Node node = new Node();
+        node.data = data;
+        node.left = left;
+        node.right = right;
+        return node;
+    }
+    public void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.println(node.data);
+            inOrder(node.right);
+        }
+    }
+
+    public void preOrder(Node node) {
+        if (node != null) {
+            System.out.println(node.data);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    public void postOrder(Node node) {
+        if (node != null) {
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.println(node.data);
+        }
+    }
+}
+
+/**
+ *
+ *  Inorder (Left, Root, Right) : 4 2 5 1 3
+ *  PreOrder (Root, Left, Right) : 1 2 4 5 3
+ *  PostOrder (Left, Right, Root) : 4 5 2 3 1
+ */
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        Tree t = new Tree();
+        Node n4 = t.makeNode(null, 4, null);
+        Node n5 = t.makeNode(null, 5, null);
+        Node n2 = t.makeNode(n4, 2, n5);
+        Node n3 = t.makeNode(null, 3, null);
+        Node n1 = t.makeNode(n2, 1, n3);
+        t.setRoot(n1);
+        t.inOrder(t.getRoot());
+        t.preOrder(t.getRoot());
+        t.postOrder(t.getRoot());
+    }
+}
